05b1e85cd2b4ebb6c8fba47e06128bd886701b5a
jianlins <jianlinshi.cn@gmail.com>
2019-12-30 16:00:24 -0700

    1.0.3a6 optimize travis build-wheels.sh

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -9,20 +9,21 @@ PLAT=${ARGS[0]}
 
 PYBINS=${ARGS[@]:1}
 # Compile wheels
-for PYBIN in PYBINS
-do
+for PYBIN in PYBINS;do
   PYBIN=/opt/python/$PYBIN/bin
   "${PYBIN}/pip" install -r /io/dev-requirements.txt
   "${PYBIN}/python" -m spacy download en
   "${PYBIN}/pip" wheel /io/ -w wheelhouse/
 done
+
 # Bundle external shared libraries into the wheels
 for whl in wheelhouse/*.whl; do
     auditwheel repair "$whl" --plat $PLAT -w /io/wheelhouse/
 done
 
 # Install packages and test
-for PYBIN in ${PYS}; do
+for PYBIN in ${PYBINS}; do
+    PYBIN=/opt/python/$PYBIN/bin
     "${PYBIN}/pip" install python-manylinux-demo --no-index -f /io/wheelhouse
     (cd "$HOME"; "${PYBIN}/nosetests" PyRuSH)
 done
