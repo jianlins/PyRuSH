7ba07ddcc1b367d40abbb062f168805cf6d7dd75
jianlins <jianlinshi.cn@gmail.com>
2019-12-30 15:50:45 -0700

    1.0.3a6 optimize travis build-wheels.sh

--- PyRuSH/version.py
+++ PyRuSH/version.py
@@ -1,3 +1,3 @@
-__version__ = '1.0.3a5'
+__version__ = '1.0.3a6'
 if __name__ == '__main__':
     print(__version__)
--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -3,13 +3,19 @@ set -e -x
 
 # Install a system package required by our library
 yum install -y atlas-devel
-PYBIN=/opt/python/$1/bin
-PLAT=$2
-# Compile wheels
-"${PYBIN}/pip" install -r /io/dev-requirements.txt
-"${PYBIN}/python" -m spacy download en
-"${PYBIN}/pip" wheel /io/ -w wheelhouse/
+ARGS=("$@")
 
+PLAT=${ARGS[0]}
+
+PYBINS=${ARGS[@]:1}
+# Compile wheels
+for PYBIN in PYBINS
+do
+  PYBIN=/opt/python/$PYBIN/bin
+  "${PYBIN}/pip" install -r /io/dev-requirements.txt
+  "${PYBIN}/python" -m spacy download en
+  "${PYBIN}/pip" wheel /io/ -w wheelhouse/
+done
 # Bundle external shared libraries into the wheels
 for whl in wheelhouse/*.whl; do
     auditwheel repair "$whl" --plat $PLAT -w /io/wheelhouse/
