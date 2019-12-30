87e355c43694d2cde4be7dd5918fae56c79aaec5
jianlins <jianlinshi.cn@gmail.com>
2019-12-30 16:55:52 -0700

    1.0.3a7 clean wheelhouse

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -13,7 +13,7 @@ echo "${PYBINS[@]}"
 for PYBIN in ${PYBINS[@]};do
   PYBIN="/opt/python/${PYBIN}/bin"
   echo "${PYBIN}"
-  "${PYBIN}/pip" install -r -q /io/dev-requirements.txt
+  "${PYBIN}/pip" install -q -r /io/dev-requirements.txt
   "${PYBIN}/python" -m spacy download en
   "${PYBIN}/pip" wheel /io/ -w wheelhouse/
 done
