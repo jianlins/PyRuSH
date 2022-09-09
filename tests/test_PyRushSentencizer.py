1b02d1104e79ba7762a12a7caebc7b0774837b46
jianlins <jianlinshi.cn@gmail.com>
2022-09-09 13:44:44 -0600

    add customized rule config in unitest

--- tests/test_PyRushSentencizer.py
+++ tests/test_PyRushSentencizer.py
@@ -80,3 +80,24 @@ I will see her in a month to six weeks.  She is to follow up with Dr. X before t
         # SpaCy has no control of sentence end. Thus, it ends up with sloppy ends.
         assert (sents[1].text == 'Ms. ABCD is a 69-year-old lady, who was admitted to the hospital with'
                                      ' chest pain and respiratory insufficiency.  ')
+
+    def test_customized_rules(self):
+        input_str = '''        
+
+
+            Ms. ABCD is a 69-year-old lady, who was admitted to the hospital with chest pain and respiratory insufficiency.  She has chronic lung disease with bronchospastic angina.
+    We discovered new T-wave abnormalities on her EKG.  There was of course a four-vessel bypass surgery in 2001.  We did a coronary angiogram. 
+
+    '''
+        from PyRuSH.RuSH import initLogger
+        initLogger()
+        nlp = English()
+        nlp.add_pipe("medspacy_pyrush", config={'rules_path':'rush_rules.tsv'})
+        doc = nlp(input_str)
+        sents = [s for s in doc.sents]
+        for sent in sents:
+            print('>' + str(sent) + '<\n\n')
+
+        # SpaCy has no control of sentence end. Thus, it ends up with sloppy ends.
+        assert (sents[1].text == 'Ms. ABCD is a 69-year-old lady, who was admitted to the hospital with'
+                                 ' chest pain and respiratory insufficiency.  ')
